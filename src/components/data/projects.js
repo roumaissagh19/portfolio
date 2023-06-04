b0bf94ca48d44f5220b0803fdee1b5db2de297a2
roumaissagh19 <romaghriballah@gmail.com>
2023-06-04 12:59:54 +0200

    first commit

---
+++ src/components/data/projects.js
@@ -0,0 +1,18 @@
+export const ProjectData=[
+    {
+        id:1,
+        title:"Grégory Jaccottet - Redesign",
+        about: "En Cours",
+        tags:["ReactJS", "TypeScript", "Ghost CMS", "Html/Css"],
+        url:"https://www.gregoryjaccottet.com",
+        
+    },
+    {
+        id:1,
+        title:"Reponsive Portfolio",
+        about: "This is a ramdom project I am using to React.JS,Javascript, HTML/CSS.This is portfolio website and I hope you are enjoying this portfolio.Make sure you like and comment if you have any doubt",
+        tags:["ReactJS", "Javascript", "Firebase", "Portfolio"],
+        
+        
+    },
+];
---
+++ src/components/data/social.js
@@ -0,0 +1,27 @@
+export const SocialData=[
+    {
+        platform:"Telegram",
+        icon: require("../../assets/icons/telegram.png"),
+        link:" https://t.me/maissa2001",
+    },
+    {
+        platform:"Github",
+        icon: require("../../assets/icons/github.png"),
+        link:" https://github.com/roumaissagh19",
+    },
+    {
+        platform:"Youtube",
+        icon: require("../../assets/icons/youtube.png"),
+         
+    },
+    {
+        platform:"Twitter",
+        icon: require("../../assets/icons/twitter.png"),
+        
+    },
+    {
+        platform:"Facebook",
+        icon: require("../../assets/icons/facebook.png"),
+        link:" https://www.facebook.com/profile.php?id=100072940620160",
+    },
+];
